9f35f89d1483fb5593943f0589faefccaffb2f88
ShaharMaaravi <shahar.maaravi@post.roni.ac.il>
2024-11-14 20:06:16 +0200

    finished HW2

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]); 
+		double sumPi = 0; 
+		double m = 1;
+		int k = 1;
+		for(int i = 0; i <= n; i++){
+			if(k == 1){
+				sumPi = sumPi + (1/m); 
+			}
+			else{
+				sumPi = sumPi - (1/m); 
+			}
+			m = m + 2;
+			k = k*-1;
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + sumPi*4);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,44 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]); 
+		char mode = args[1].charAt(0);
+		int counter = 0;
+		int current = n; 
+		
+
+		for(int i = 1; i <= n; i++) {
+			current = i;
+			counter = 1;
+			if(current == 1 & mode == 'v'){
+				System.out.print(current + ", ");
+				if(current%2 == 0){ 
+					current = current/2; 
+					counter++;
+				}
+				else{
+					current = current*3 + 1; 
+					counter++;
+				}
+			}
+			while(current > 1){
+				if(mode == 'v'){
+					System.out.print(current + ", ");
+				}
+				if(current%2 == 0){ 
+					current = current/2; 
+					counter++;
+				}
+				else{
+					current = current*3 + 1; 
+					counter++;
+				}
+			}
+			if(mode == 'v'){
+				System.out.print(current + " (" + counter + ")");
+				System.out.println("");
+			}
+		}
+		System.out.println("Every one of the first " + n + " hailstone sequences reached 1");
 	}
 }
